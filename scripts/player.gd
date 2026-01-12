60f8d1a5b5b814db3cbd2540f550d32345daba29
Nathan <nmfightmaster@gmail.com>
2026-01-12 06:30:56 -0800

    update visuals

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,11 +3,13 @@ extends CharacterBody2D
 @export var move_speed: float = 200.0
 @export var turn_speed: float = 2.0
 
+@onready var tank_body: Node2D = $TankBodyMesh
+
 func _physics_process(delta: float) -> void:
     var rotation_direction := Input.get_axis("turn_counter_clockwise", "turn_clockwise")
-    rotation += rotation_direction * turn_speed * delta
+    tank_body.rotation += rotation_direction * turn_speed * delta
 
     var move_direction := Input.get_axis("move_backward", "move_forward")
-    velocity = Vector2.UP.rotated(rotation) * move_direction * move_speed
+    velocity = Vector2.UP.rotated(tank_body.rotation) * move_direction * move_speed
     
     move_and_slide()
