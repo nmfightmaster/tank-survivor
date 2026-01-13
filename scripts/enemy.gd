78d3bb7f3d10493f66c6e27f3ace99796a6f06e3
Nathan Fightmaster <nmfightmaster@gmail.com>
2026-01-13 11:51:34 -0800

    shooting

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,15 +2,29 @@ extends CharacterBody2D
 
 @export var move_speed: float = 180.0
 @export var turn_speed: float = 2.0
+@export var max_health: int = 50
+
+@export_group("Combat")
+@export var missile_scene: PackedScene = preload("res://scenes/missile.tscn")
+@export var fire_rate: float = 1.0
+@export var projectile_damage: int = 5
+@export var projectile_speed: float = 300.0
+
+var health: int
+var reload_timer: float = 0.0
 
 @onready var tank_body: Node2D = $TankBodyMesh
-
 @onready var tank_turret: Node2D = $TankTurretSprite
+@onready var muzzle: Marker2D = $TankTurretSprite/Muzzle
 
 func _ready() -> void:
 	add_to_group("enemy")
+	health = max_health
 
 func _physics_process(delta: float) -> void:
+	if reload_timer > 0:
+		reload_timer -= delta
+
 	var player = get_tree().get_first_node_in_group("player")
 	if not player:
 		# Fallback try to find sibling named Player
@@ -27,4 +41,18 @@ func _physics_process(delta: float) -> void:
 		tank_turret.look_at(player.global_position)
 		tank_turret.rotation += PI/2
 		
+		if reload_timer <= 0:
+			shoot()
+		
 		move_and_slide()
+
+func shoot() -> void:
+	if missile_scene:
+		var missile = missile_scene.instantiate()
+		get_parent().add_child(missile)
+		missile.global_position = muzzle.global_position
+		missile.rotation = tank_turret.global_rotation
+		missile.speed = projectile_speed
+		missile.damage = projectile_damage
+		missile.shooter = self
+		reload_timer = fire_rate
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,12 +3,28 @@ extends CharacterBody2D
 @export var move_speed: float = 200.0
 @export var turn_speed: float = 2.0
 @export var turret_turn_speed: float = 4.0
+@export var max_health: int = 100
+
+@export_group("Combat")
+@export var missile_scene: PackedScene = preload("res://scenes/missile.tscn")
+@export var fire_rate: float = 0.5
+@export var projectile_damage: int = 10
+@export var projectile_speed: float = 400.0
+
+var health: int
+var reload_timer: float = 0.0
 
 @onready var tank_body: Node2D = $TankBodyMesh
-
 @onready var tank_turret: Sprite2D = $TankTurretSprite
+@onready var muzzle: Marker2D = $TankTurretSprite/Muzzle
+
+func _ready() -> void:
+	health = max_health
 
 func _physics_process(delta: float) -> void:
+	if reload_timer > 0:
+		reload_timer -= delta
+
 	var rotation_direction := Input.get_axis("turn_counter_clockwise", "turn_clockwise")
 	tank_body.rotation += rotation_direction * turn_speed * delta
 
@@ -17,8 +33,8 @@ func _physics_process(delta: float) -> void:
 	
 	# Turret aiming
 	var enemies = get_tree().get_nodes_in_group("enemy")
+	var nearest_enemy = null
 	if enemies.size() > 0:
-		var nearest_enemy = null
 		var min_dist = INF
 		
 		for enemy in enemies:
@@ -30,5 +46,20 @@ func _physics_process(delta: float) -> void:
 		if nearest_enemy:
 			var target_phase = global_position.direction_to(nearest_enemy.global_position).angle()
 			tank_turret.rotation = lerp_angle(tank_turret.rotation, target_phase + (PI/2), turret_turn_speed * delta)
-	
+			
+			# Auto-fire
+			if reload_timer <= 0:
+				shoot()
+
 	move_and_slide()
+
+func shoot() -> void:
+	if missile_scene:
+		var missile = missile_scene.instantiate()
+		get_parent().add_child(missile)
+		missile.global_position = muzzle.global_position
+		missile.rotation = tank_turret.global_rotation
+		missile.speed = projectile_speed
+		missile.damage = projectile_damage
+		missile.shooter = self
+		reload_timer = fire_rate
